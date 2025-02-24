1dabad448cbb92b29ef9bdcdc281ee109e152a0d
chanbin <rcq0111@gmail.com>
2025-02-24 16:30:11 +0900

    v1.01day11 pandas  s chlearn custom LinearRegression

---
+++ chlearn.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self):
+        self.slope = None
+        self.intercept = None
+
+    def fit(self ,X, y):
+        """
+        learning function
+        :param X: independent variable (2d array format)
+        :param y: dependent variable (2d array format)
+        :return: void
+        """
+        X_mean = np.mean(X)
+        y_mean = np.mean(y)
+
+        denominator = np.sum(pow(X-X_mean, 2)) # 분모구하는 공식 / 관측값에서 타겟(mean값)을 뺀값 / 제곱을 한 이유 : 음수 방지
+        numerate = np.sum((X-X_mean)*(y-y_mean)) # 분자구하는 공식
+
+        self.slope = numerate / denominator # 기울기
+        self.intercept = y_mean - (self.slope * X_mean) # 절편
+
+    def predict(self, X) -> np.ndarray:
+        """
+        predict value for input
+        :param X: new independent variable
+        :return: predict value for input (2d array format)
+        """
+        return self.slope * np.array(X) + self.intercept
--- day11.py
+++ day11.py
@@ -1,13 +1,10 @@
-#from statistics import LinearRegression
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
+from chlearn import LinearRegression # 커스텀 모델 생성
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
 
 # 데이터를 다운로드하고 준비합니다.
 ls = pd.read_csv("https://github.com/ageron/data/raw/main/lifesat/lifesat.csv/")
-#print(type(ls))
-#print(ls)
 X = ls[["GDP per capita (USD)"]].values
 y = ls[["Life satisfaction"]].values
 #print(y)
@@ -15,22 +12,16 @@ y = ls[["Life satisfaction"]].values
 # 데이터를 그래프로 나타냅니다.
 ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction") # grid=True는 격자
 plt.axis([23500, 62500, 4, 9]) # asis는 x, y축에대한 사이즈 설정
-#plt.show()
+plt.show()
 
 # 선형 모델을 선택합니다.
-#model = LinearRegression()
-model = KNeighborsRegressor(n_neighbors=3) # n_neighbors : hyperparameter 학습하는 동안 적용할 규제의 양
+model = LinearRegression()
 
 # 모델을 훈련합니다.
 model.fit(X, y)
 
-# 키프로스에 대해 예측을 만듭니다.
-X_new = [[37655.2]] # 2020년 키프로스 1인당 GDP
-print(model.predict(X_new)) # 출력 : [[6.30165767]]
-# LinearRegression : 6.30165767
-# KNeighborsRegressor : 6.33333333 / k-초근접 이웃 k-nn (k-nearest neighbors)
 
-X_new2 = [[31721.3]]
-print(model.predict(X_new2))
+X_new = [[31721.3]]
+print(model.predict(X_new))
 # LinearRegression : 5.89940454 -> 실제 값과 더 가까움
 # KNeighborsRegressor : 5.7
