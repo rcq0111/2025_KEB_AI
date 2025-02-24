3df49ad63c766ee0a12737d7af6a127703e86397
chanbin <rcq0111@gmail.com>
2025-02-24 15:54:07 +0900

    v1.0 day11 pandas ㅗ hyperparameter ex1

--- day11.py
+++ day11.py
@@ -17,11 +17,14 @@ ls.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfactio
 plt.axis([23500, 62500, 4, 9]) # asis는 x, y축에대한 사이즈 설정
 #plt.show()
 
+# 선형 모델을 선택합니다.
 #model = LinearRegression()
-model = KNeighborsRegressor(n_neighbors=3)
+model = KNeighborsRegressor(n_neighbors=3) # n_neighbors : hyperparameter 학습하는 동안 적용할 규제의 양
+
+# 모델을 훈련합니다.
 model.fit(X, y)
 
-# 키프로스에 대해 예츳을 만듭니다.
+# 키프로스에 대해 예측을 만듭니다.
 X_new = [[37655.2]] # 2020년 키프로스 1인당 GDP
 print(model.predict(X_new)) # 출력 : [[6.30165767]]
 # LinearRegression : 6.30165767
