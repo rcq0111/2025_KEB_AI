bbd122290bbc2053ad31c075b7a0b0e03c500cb4
chanbin <rcq0111@gmail.com>
2025-02-24 13:40:23 +0900

    v0.67day11 pandas ãmethod chaining 22line

--- day11.py
+++ day11.py
@@ -9,5 +9,11 @@ df =pd.DataFrame(
 )
 print(df)
 
-df2 = pd.melt(df).rename(columns={'variable' : 'var', 'value' : 'val'}).query('val >= 10')
-print(df2)
+#df3 = df.iloc[0:2]
+#df3 =df.loc[1:2]
+df3 = df.iloc[:, [0,2]]
+print(len(df3))
+
+# df2 = pd.melt(df).rename(columns={'variable' : 'var', 'value' : 'val'}).query('val >= 10').sort_values('val', ascending=False)
+#
+# print(df2)
