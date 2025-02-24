6145b26bbf67fdfdc5f7f0d51cd939329fcada4a
chanbin <rcq0111@gmail.com>
2025-02-24 13:27:31 +0900

    v0.6 day11 pandas melt query

--- day11.py
+++ day11.py
@@ -1,15 +1,13 @@
-# Series : 1차원 배열 형태의 자류구조로, 인덱스와 값으로 이루어져 있다
-
-# DataFrame : 2차원 태이블 형태의 자류구조로, 여러개에 시리즈 객체가 모여서 열과 행으로 구성
-
-import numpy as np
 import pandas as pd
 
-s = pd.Series([1, 2, 3, 4], index=['a', 'b', 'c', 'd'])
-print(s)
+df =pd.DataFrame(
+    [[4, 7 ,10],
+         [5, 88 ,11],
+         [16, 9 ,12]],
+         index=(1, 2, 3),
+         columns=('a', 'b', 'c')
+)
+print(df)
 
-s2 = pd.Series([99, 100, 98, 91, 92])
-s2_subject = s2[1:4]
-print(s2_subject)
-s2_mean = s2.mean()
-print(s2_mean, s2.min())
+df2 = pd.melt(df).rename(columns={'variable' : 'var', 'value' : 'val'}).query('val >= 10')
+print(df2)
