d2688a49188718214955111d0891371c87ab4d56
chanbin <rcq0111@gmail.com>
2025-02-24 11:37:44 +0900

    v0.5 day11 pandas DataFrame

--- day11.py
+++ day11.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 
 df =pd.DataFrame(
-    {"a" : [4, 5, 6],
-          "b" : [7, 8, 9],
-          "c" : [10, 11, 12]},index = [1, 2, 3]
+    [[4, 7 ,10],
+         [5, 8 ,11],
+         [6, 9 ,12]],
+         index=(1, 2, 3),
+         columns=('a', 'b', 'c')
 )
 print(df)
