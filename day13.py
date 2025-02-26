d3c72c9e532cb2c7868916f96df3368954f26dd4
chanbin <rcq0111@gmail.com>
2025-02-26 18:17:39 +0900

    v0.8 day13 ã3 test

--- day13.py
+++ day13.py
@@ -1,21 +1,26 @@
-from pandas.core.indexes.multi import names_compat
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+mpg = sns.load_dataset('mpg')
+#print(mpg.info())
+#print(mpg.tail())
+
+mpg.dropna(subset=["horsepower"], inplace=True) # horsepower=None 인 6행 삭제 / p102
+#print(mpg.info())
+
+# 방법 1) origin drop
+# mpg.drop("origin", axis=1, inplace=True)
+# print(mpg.info())
+
+# 방법 1) origin encoded
 
 
-class Pokemon:
-    def __init__(self, name):
-        self.name = name
-    def walk(self):
-        print("walk..")
-    def attack(self, target_Pokemon):
-        print(f"{self.name} attack!!{target_Pokemon.name}")
+mpg.drop("name", axis=1, inplace=True) # 연비와 전혀 관련 없는 이름은 drop
+print(mpg.info())
 
-class Pikachu(Pokemon):
-    pass
 
-class Agumon:
-    def __init__(self, name):
-        self.name = name
 
-agumon = Agumon('아구몬')
-pikachu = Pikachu('피카츄')
-pikachu.attack(agumon)
